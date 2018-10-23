d7214b0eb06c0f0689cf483d5d07db6a51191891
Ilya Sysoi <14ilya12322@gmail.com>
2018-10-23 12:56:40 +0300

    add new task

---
+++ FirstCourseFinalTask/FirstCourseFinalTask/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  FirstCourseFinalTask
+//
+//  Copyright © 2017 E-Legion. All rights reserved.
+//
+
+import Foundation
+import FirstCourseFinalTaskChecker
+
+
+
+
+let checker = Checker(usersStorageClass: <#T##UsersStorageProtocol.Type#>,
+                      postsStorageClass: <#T##PostsStorageProtocol.Type#>)
+checker.run()
+
